df16e3b16af540a1252c0f620238b0816a8e2311
DGroeg <DGroeg@users.noreply.github.com>
2015-03-25 14:44:09 +0100

    Create arbitre.h
    Le .h.

---
+++ arbitre.h
@@ -0,0 +1,8 @@
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include "backgammon.h"
+
+int IsMatchOver(SGameState *gameState);
+int IsGameOver(SGameState *gameState);
+SGameState InitState();
