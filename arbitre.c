8cadeaa794fae1400aa5f929227c24eaf3c4bc31
DGroeg <DGroeg@users.noreply.github.com>
2015-03-26 00:19:11 +0100

    Update arbitre.c

--- arbitre.c
+++ arbitre.c
@@ -5,11 +5,11 @@
 
 int IsMatchOver(SGameState *gameState)
 {
-  if (/*pions blancs sur last case = 15)*/)
+  if (gameState->bar[0]==15)/*pions noirs sur last case = 15*/
   {
 	return 0;
   }
-  else if (/*pions noirs sur last case = 15)*/)
+  else if (gameState->bar[1]==15)/*pions blancs sur last case = 15*/
   {
 	return 1;
   }
@@ -21,11 +21,11 @@ int IsMatchOver(SGameState *gameState)
 
 int IsGameOver(SGameState *gameState)
 {
-  if (gameState.whiteScore==3)
+  if (gameState->whiteScore==3)
   {
 	return 0;
   }
-  else if (gameState.blackScore==3)
+  else if (gameState->blackScore==3)
   {
 	return 1;
   }
@@ -35,11 +35,34 @@ int IsGameOver(SGameState *gameState)
   }
 }
 
-SGameState InitState()              /*  24 ---- 13
-					                	             1 ---- 12	*/
+SGameState* InitState()              /*  12 ---- 23
+					                	            11 ---- 0	  VOIR IMAGE WIKIPEDIA */
 {
-  SGameState gameState;
-  gameState->
+  SGameState* gameState;
+  gameState->board[0].owner=1;
+  gameState->board[0].nbDames=2;
+  gameState->board[11].owner=1;
+  gameState->board[11].nbDames=5;
+  gameState->board[16].owner=1;
+  gameState->board[16].nbDames=3;
+  gameState->board[18].owner=1;
+  gameState->board[18].nbDames=5;
+  gameState->board[5].owner=0;
+  gameState->board[5].nbDames=5;
+  gameState->board[7].owner=0;
+  gameState->board[7].nbDames=3;
+  gameState->board[12].owner=0;
+  gameState->board[12].nbDames=5;
+  gameState->board[23].owner=0;
+  gameState->board[23].nbDames=2;
+  gameState->whiteScore=0;
+  gameState->blackScore=0;
+  //gameState->bar[0]->; quest ce que bar ? surement les cases de victoire
+  //gameState->out[0]->; quest ce que out ? surement les cases ou vont ceux qui sont bouffés
+  //gameState->bar[1]->;
+  //gameState->out[1]->;
+  gameState->turn=0;
+  gameState->stake=1;
 
   return gameState;
 }
