645fcf4ed7a7cb21c390add157a2c3ccc4dfde10
DGroeg <DGroeg@users.noreply.github.com>
2015-03-30 15:37:21 +0200

    Update arbitre.h

--- arbitre.h
+++ arbitre.h
@@ -7,3 +7,4 @@ int IsMatchOver(SGameState *gameState);
 int IsGameOver(SGameState *gameState);
 SGameState InitState();
 unsigned int* rollDice(int nbDice);
+SMoves[2] emptyMoves(SMoves moves, int player);
