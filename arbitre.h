26d3606c4ba71b8a7d209730043178468037c1eb
DGroeg <DGroeg@users.noreply.github.com>
2015-03-31 14:58:42 +0200

    Update arbitre.h

--- arbitre.h
+++ arbitre.h
@@ -5,6 +5,7 @@
 
 int IsMatchOver(SGameState *gameState);
 int IsGameOver(SGameState *gameState);
-SGameState InitState();
-unsigned int* rollDice(int nbDice);
-SMoves[2] emptyMoves(SMoves moves, int player);
+SGameState* InitBoard();
+SGameState* InitState();
+unsigned int* rollDice(unsigned int* dice);
+SMoves[2] emptyMoves(SMoves moves); //attention !!!!!!! que se passe-t-il si moves fait 4 cases !!!!!!!!!
