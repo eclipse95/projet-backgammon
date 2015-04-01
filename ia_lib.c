4de0074fe519639c302e49909fafad30e0f73e54
Fibri <kevin.coepeau@gmail.Com>
2015-04-01 15:42:52 +0200

    Update ia_lib.c

--- ia_lib.c
+++ ia_lib.c
@@ -5,7 +5,7 @@
 void InitLibrary(char name[50])
 {
 	printf("InitLibrary\n");
-	strcpy(name,"AKGBM");
+	strcpy(name,"Yay");
 }
 
 void StartMatch(const unsigned int target_score)
@@ -43,4 +43,36 @@ int TakeDouble(const SGameState * const gameState)
 void PlayTurn(const SGameState * const gameState, const unsigned char dices[2], SMove moves[4], unsigned int *nbMove, unsigned int tries)
 {
 	printf("PlayTurn\n");
+	
+	//generate All Moves Possible
+
+    	//get Score
+
+    	//return first
+}
+
+// Unofficial Functions
+SMove* getAllMovements(const SGameState* const gameState, const unsigned char dices[2]){
+	SMove* allMovements;
+	
+	
+	
+	return allMovements;
+}
+
+int* getScore(SMove* allMovements){
+	int* scores;
+	
+	return scores;
+}
+
+int getBest(int* scores){
+	int max = -1;
+	int id = -1;
+	int length = sizeof(scores)/sizeof(int);
+	
+	
+	
+	
+	return id;
 }
