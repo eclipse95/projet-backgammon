40df18b7db737508f70772d1ed9d46e6a7ad535d
Fibri <kevin.coepeau@gmail.Com>
2015-04-02 20:31:24 +0200

    Ajout de la liste chainee

---
+++ pile.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Maillon Maillon;
+
+// IAMove
+
+struct Maillon{
+    Maillon* prec;
+    IAMove* movement;
+    Maillon* suiv;
+};
+
+typedef struct{
+    Maillon* first;
+    Maillon* last;
+    int size;
+}Pile;
+
+/* Constructeur de pile */
+Pile* createPile();
+
+/* Constructeur de maillon */
+Maillon* createMaillon();
+
+short estVide(Pile* pile);
+
+void push(Pile*, IAMove*);
+
+void pop(Pile*);
+
+IAMove* top(Pile*);
+
+
+int testPile();
+
