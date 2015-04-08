e093e30323af1766002c5b98f46404702dc4f036
KnightLink <twilightp@hotmail.fr>
2015-04-09 00:16:41 +0200

    Avancement du jour : se lance, display les pions "correctement" via un gamestate. Partie affichage quasi finie

--- interface.c
+++ interface.c
@@ -5,6 +5,63 @@
 #include "backgammon.h"
 #include <SDL2/SDL.h>
 #include <math.h>
+#include <assert.h>
+
+#define TAILLE_PION 40
+#define X_WINDOW 800
+#define Y_WINDOW 600
+
+/* TODO
+ * transparence ? sans tout changer si possible. A essayer : setcolorkey sur les surfaces dans le loader
+ * display dice
+ * display stake
+ * display out
+ * display bar
+ */
+
+/* A VIRER QUAND L'ARBITRE MARCHE */
+void InitBoard(SGameState* gameState)
+{
+	int i;
+	for (i=0; i<12; i++)
+	{
+		switch(i)
+		{
+		case 0 :
+			gameState->board[i].nbDames=2;
+			gameState->board[i].owner=WHITE;
+			gameState->board[23-i].nbDames=2;
+			gameState->board[23-i].owner=BLACK;
+			break;
+		case 5 :
+			gameState->board[i].nbDames=5;
+			gameState->board[i].owner=BLACK;
+			gameState->board[23-i].nbDames=5;
+			gameState->board[23-i].owner=WHITE;
+			break;
+		case 7 :
+			gameState->board[i].nbDames=3;
+			gameState->board[i].owner=BLACK;
+			gameState->board[23-i].nbDames=3;
+			gameState->board[23-i].owner=WHITE;
+			break;
+		case 11 :
+			gameState->board[i].nbDames=5;
+			gameState->board[i].owner=WHITE;
+			gameState->board[23-i].nbDames=5;
+			gameState->board[23-i].owner=BLACK;
+			break;
+		default:
+			gameState->board[i].nbDames=0;
+			gameState->board[i].owner=NOBODY;
+			gameState->board[23-i].nbDames=0;
+			gameState->board[23-i].owner=NOBODY;
+			break;
+		}
+	}
+	gameState->stake=1;
+	gameState->turn=0;
+}
 
 // MODIF POSSIBLE : Passer le tableau de SMove en paramètre parce que le code est hyper gourmand ; Je sais pas si vous voulez utiliser ça donc je m'avance pas
 // Possibles causes de bug :
@@ -44,36 +101,114 @@ SMove* getAllPossibleMoves(SGameState *gameState, int player, unsigned char dice
 	return moves;
 }
 
+// getXSquare et getYSquare retournent la position du premier pion à afficher en fonction de la case renseignée
 int getXSquare(int id)
 {
+	if(id>=18 && id<=23)
+	{
+		return (392 + 60*(23-id));
+	}
+	if(id>=12 && id<=17)
+	{
+		return (18 + 60*(17-id));
+	}
+	if(id>=6 && id<=11)
+	{
+		return (321 - 60*(12-id));
+	}
+	if(id>=0 && id<=5)
+	{
+		return (695 - 60*(5-id));
+	}
 	return 0;
 }
 int getYSquare(int id)
 {
+	if(id>=12 && id<=23)
+	{
+		return 16;
+	}
+	if(id>=0 && id<=11)
+	{
+		return 535;
+	}
 	return 0;
 }
-void drawSquare(SGameState *gameState, int id)
+
+// dessine tous les pions de la case id
+void drawSquare(SGameState *gameState, int id, SDL_Texture* tabSprite[], SDL_Renderer* renderer)
 {
 	int x=getXSquare(id);
 	int y=getYSquare(id);
+	printf("drawsquare x:%i y:%i\n",x,y);
+	int i,multiplier;
+	if(id>=0 && id<=11){multiplier=-1;}
+	if(id>=12 && id<=23){multiplier=1;}
+	SDL_Texture* texture=NULL;
+	Square square=gameState->board[id];
+	if(x==0 || y==0){printf("coordonnées introuvables");return;}
+	switch(square.owner)
+	{
+		case BLACK:
+			printf("case black\n");
+			texture=tabSprite[1];
+			break;
+		
+		case WHITE:
+			printf("case white\n");
+			texture=tabSprite[2];
+			break;
+			
+		case NOBODY:
+			printf("case nobody\n");
+			return;
+			break;
+	}
+	for(i=0;i<square.nbDames;i++)
+	{
+		printf("Tour de boucle affiche dames %i\n",i);
+		SDL_Rect dest={x,y+i*TAILLE_PION*multiplier,TAILLE_PION,TAILLE_PION};
+		SDL_RenderCopy(renderer,texture,NULL,&dest);
+	}
 }
-void drawMultiplier(SGameState *gameState)
+
+// dessine le videau
+void drawMultiplier(SGameState *gameState,SDL_Texture* tabSprite[], SDL_Renderer* renderer)
 {
-	return 0;
+	printf("drawmultiplier\n");
 }
-void drawDice(SGameState *gameState,unsigned char dice[2])
+
+// dessine les dés
+void drawDice(SGameState *gameState,unsigned char dice[2],SDL_Texture* tabSprite[], SDL_Renderer* renderer)
 {
-	return 0;
+	printf("drawdice\n");
 }
-void drawAll(SGameState *gameState)
+
+// dessine tout
+void drawAll(SGameState *gameState, SDL_Texture* tabSprite[], SDL_Renderer* renderer)
 {
+	printf("drawall\n");
 	int i;
+	unsigned char dice[2]; // à enlever plus tard
 	for(i=0;i<24;i++)
 	{
-		drawSquare(gameState,i);
+		printf("tour boucle drawsquare %i\n",i);
+		drawSquare(gameState,i,tabSprite,renderer);
 	}
-	drawMultiplier(gameState);
-	drawDice(gameState);
+	drawMultiplier(gameState,tabSprite,renderer);
+	drawDice(gameState,dice,tabSprite,renderer);
+}
+
+// fonction custom pour automatiser le process de création de textures
+SDL_Texture* textureLoader(char* filename,SDL_Surface* surfaceLoader, SDL_Renderer* renderer)
+{
+	printf("load texture %s\n",filename);
+	SDL_Texture* returnValue;
+	surfaceLoader=SDL_LoadBMP(filename);
+	returnValue=SDL_CreateTextureFromSurface(renderer,surfaceLoader);
+	SDL_FreeSurface(surfaceLoader);
+	SDL_SetTextureBlendMode(returnValue, SDL_BLENDMODE_BLEND);
+	return returnValue;
 }
 
 int main(int argc, char** argv)
@@ -83,31 +218,31 @@ int main(int argc, char** argv)
         fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
         return -1;
     }
-
+	int i;
+	SGameState* gameState=malloc(sizeof(SGameState));
+	InitBoard(gameState);
 	/* Création de la fenêtre */
 	SDL_Window* pWindow = NULL;
 	pWindow = SDL_CreateWindow("Backgammon",SDL_WINDOWPOS_UNDEFINED,
 															  SDL_WINDOWPOS_UNDEFINED,
-															  800,
-															  600,
+															  X_WINDOW,
+															  Y_WINDOW,
 															  SDL_WINDOW_SHOWN);
 
-	SDL_Renderer *pRenderer = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED); // Création
-	SDL_Surface* pSprite = SDL_LoadBMP("./img/backgammon.bmp");
-	if(pSprite==NULL)
-	{
-		printf("BMP loading failed : %s\n",SDL_GetError());
-		return 1;
-	}
-	
-	SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer,pSprite); 
-	SDL_Rect dest = { 0,0, pSprite->w, pSprite->h};
-	SDL_RenderCopy(pRenderer,pTexture,NULL,&dest); // Copie du sprite
+	SDL_Renderer *pRenderer = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED); // Init renderer
+	// Bloc de génération initiale des textures à utiliser
+	SDL_Texture* ptabSprite[9];
+	SDL_Surface* surfaceLoader;
+	ptabSprite[0]=textureLoader("./img/backgammon.bmp",surfaceLoader,pRenderer);
+	ptabSprite[1]=textureLoader("./img/black.bmp",surfaceLoader,pRenderer);
+	ptabSprite[2]=textureLoader("./img/white.bmp",surfaceLoader,pRenderer);
+	SDL_Rect dest = { 0,0, X_WINDOW,Y_WINDOW};
+	SDL_RenderCopy(pRenderer,ptabSprite[0],NULL,NULL); // Copie du sprite
+	drawAll(gameState,ptabSprite,pRenderer);
 	SDL_RenderPresent(pRenderer); // Affichage
 	SDL_Delay(3000);
-	
-	SDL_DestroyTexture(pTexture); // Libération de la mémoire
-	SDL_FreeSurface(pSprite); // Libération de la ressource
+	for(i=0;i<9;i++)
+		SDL_DestroyTexture(ptabSprite[i]); // Libération de la mémoire
 	SDL_DestroyRenderer(pRenderer); // Libération de la mémoire
 
     SDL_Quit();
--- interface.h
+++ interface.h
@@ -1,7 +1,8 @@
 SMove* getAllPossibleMoves(SGameState *gameState, int player, unsigned char dice[2]);
-int drawSquare(SGameState *gameState, int id);
+void drawSquare(SGameState *gameState, int id, SDL_Texture* tabSprite[], SDL_Renderer* renderer);
 int getXSquare(int id);
 int getYSquare(int id);
-int drawMultiplier(SGameState *gameState);
-int drawDice(SGameState *gameState,unsigned char dice[2]);
-int drawAll(SGameState *gameState);
+void drawMultiplier(SGameState *gameState,SDL_Texture* tabSprite[], SDL_Renderer* renderer);
+void drawDice(SGameState *gameState,unsigned char dice[2],SDL_Texture* tabSprite[], SDL_Renderer* renderer);
+void drawAll(SGameState *gameState, SDL_Texture* tabSprite[], SDL_Renderer* renderer);
+SDL_Texture* textureLoader(char* filename,SDL_Surface* surfaceLoader,SDL_Renderer* renderer);
